64e60f6a3e8dc000e9fb25221d76a8118dee583f
Mochamad Syarief Maulana <alanmsmxyz@gmail.com>
2020-04-25 13:11:08 +0700

    Initial commit.

---
+++ src/kopikopi/main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kopikopi;
+
+/**
+ *
+ * @author alanmsmxyz
+ */
+public class main {
+    public static void main(String args[]) {
+        switch (args[0]) {
+            case "editMenu":
+                // call admin handler
+                break;
+            case "barista":
+                // call barista handler
+                break;
+            default:
+                // call customer handler
+                break;
+        }
+    }
+}
